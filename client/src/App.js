1ed92ca8d360c6340f4846297dcf15f381a1abe0
Leandro Bueno Santos <leandro.bueno@globo.com>
2023-08-05 23:37:22 -0400

    Small change

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Calculate Fibonacci React App
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
