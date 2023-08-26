13c092f83775b65d105a3f124c5632b54d2f59dd
Leandro Bueno Santos <leandro.bueno@globo.com>
2023-08-26 14:48:19 -0400

    bump up version

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Calculate Fibonacci React App
+            Calculate Fibonacci React App v1
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
